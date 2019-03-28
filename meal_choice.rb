5de05cb68324ac26976c74aebfebc56ad749d469
Unsar Malik <unsar.malik24@gmail.com>
2019-03-28 02:56:30 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,10 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+#meal_choice
+def meal_choice(arg="meat")
+  "#{arg}"
+end
+
+meal_choice
+meal_choice("vegan")
